b6bcf6abe8a5ccef05225d49e7132f7ae31d7faa
burak-odabasi <burakodabasi.dev@gmail.com>
2024-02-05 04:22:49 +0300

    dark theme added

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log(
+  window
+    .getComputedStyle(document.documentElement)
+    .getPropertyValue("--div-background-color")
+);
+
+document.getElementById("dark-theme-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "#333");
+});
+document.getElementById("light-theme-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "#90EE90");
+});
